d3f8a5b526e5a9b9c8965d52969dca1fc1817480
unknown <sumit.chaudhary29ag@gmail.com>
2025-08-31 11:06:33 +0530

    update

--- vhinternational/settings.py
+++ vhinternational/settings.py
@@ -166,16 +166,17 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
 }
-AWS_ACCESS_KEY_ID = os.environ.get("A_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("A_SECRET")
-AWS_STORAGE_BUCKET_NAME = "sumittestbucket2908"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME") 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_SIGNATURE_NAME='s3v4'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-AWS_S3_REGION_NAME =  'us-east-1'
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME") 
 
-AWS_S3_CUSTOM_DOMAIN = f'sumittestbucket2908.s3.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
