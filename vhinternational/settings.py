052d1a8e72867f6e6931edaee6f3d76dbaf72080
vhinternationaluk <vhinternationaluk@gmail.com>
2025-09-21 21:49:03 +0530

    Update settings.py

--- vhinternational/settings.py
+++ vhinternational/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'http://localhost:8080/']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
